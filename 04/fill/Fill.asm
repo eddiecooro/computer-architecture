08df994f578fac25af183c7dfc811113baa969e7
Ali Najafi <najafi1998ali@gmail.com>
2019-10-25 21:09:00 +0330

    Update Fill.asm

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -1,14 +1,65 @@
-// This file is part of www.nand2tetris.org
-// and the book "The Elements of Computing Systems"
-// by Nisan and Schocken, MIT Press.
-// File name: projects/04/Fill.asm
+// Make Screen become black when any keyboard's button clicked!
 
-// Runs an infinite loop that listens to the keyboard input.
-// When a key is pressed (any key), the program blackens the screen,
-// i.e. writes "black" in every pixel;
-// the screen should remain fully black as long as the key is pressed. 
-// When no key is pressed, the program clears the screen, i.e. writes
-// "white" in every pixel;
-// the screen should remain fully clear as long as no key is pressed.
+//----------------------- Reading keyboard
+@READ
+0;JMP
+(READ)
+	@KBD
+	D=M
 
-// Put your code here.
+	@WHITE
+	D;JEQ
+
+	@BLACK 
+	D;JNE
+//-----------------------  (16384 - 8191)
+
+
+(WHITE)
+	@16384
+	D=A
+	@addr
+	M=D
+
+	(LOOP1)
+	@addr
+	A=M
+	M=0
+
+	@addr
+	M=M+1
+	@8191
+	D=A
+	@addr
+	D=D-M
+	@LOOP1
+	D;JNE
+
+	@READ
+	0;JMP
+
+
+
+
+(BLACK)
+	@16384
+	D=A
+	@addr
+	M=D
+
+	(LOOP2)
+	@addr
+	A=M
+	M=-1
+
+	@addr
+	M=M+1
+	@8191
+	D=A
+	@addr
+	D=D-M
+	@LOOP2
+	D;JNE
+
+	@READ
+	0;JMP
